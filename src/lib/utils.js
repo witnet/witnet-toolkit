350ac572ff137240516f12acfb8ab03ad8844d68
Guillermo Díaz <guillermo@otherplane.com>
2025-01-13 10:38:34 +0100

    chore(lib): rearrange helpers vs utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,19 +2,34 @@ const cbor = require("cbor")
 const os = require("os")
 const crypto = require("crypto")
 const { exec } = require("child_process")
+const net = require("net")
+const helpers = require("./helpers")
 
 var protoBuf = require("protobufjs")
-var protoRoot = protoBuf.Root.fromJSON(require("../assets/witnet.proto.json"))
+var protoRoot = protoBuf.Root.fromJSON(require("../../witnet/witnet.proto.json"))
 var RADRequest = protoRoot.lookupType("RADRequest")
 
-import { RadonRequest } from "./lib/radon/artifacts"
-import { RadonRetrieval } from "./lib/radon/retrievals"
-import { RadonReducer } from "./lib/radon/reducers"
-import { RadonFilter } from "./lib/radon/filters"
+import { RadonRequest } from "./radon/artifacts"
+import { RadonRetrieval } from "./radon/retrievals"
+import { RadonReducer } from "./radon/reducers"
+import { RadonFilter } from "./radon/filters"
 import { 
-  RadonAny, RadonArray, RadonBoolean, RadonBytes, RadonFloat, RadonInteger, RadonMap, RadonString,
+  RadonAny, 
+  RadonArray, 
+  RadonBoolean, 
+  RadonBytes, 
+  RadonFloat, 
+  RadonInteger, 
+  RadonMap, 
+  RadonString,
   RadonOperators
- } from "./lib/radon/types"
+} from "./radon/types"
+
+export { 
+  fromHexString, isHexString, isHexStringOfLength, toHexString,
+  parseURL, ipIsPrivateOrLocalhost,
+  toUtf8Array, utf8ArrayToStr,
+} from "./helpers"
 
 export function decodeRequest(hexString) {
   const buffer = fromHexString(hexString)
@@ -51,8 +66,18 @@ export function decodeScript(hexString) {
   return parseScript(array)
 }
 
+export function encodeRequest(payload) {
+  var errMsg = RADRequest.verify(payload)
+  if (errMsg) {
+    throw Error(errMsg);
+  } else {
+    var message = RADRequest.fromObject(payload);
+    return RADRequest.encode(message).finish()
+  }
+}
+
 export async function execDryRun(bytecode, ...flags) {
-  if (!isHexString(bytecode)) {
+  if (!helpers.isHexString(bytecode)) {
     throw EvalError("Witnet.Utils.execDryRun: invalid bytecode")
   } else {
     const npx = os.type() === "Windows_NT" ? "npx.cmd" : "npx"
@@ -69,23 +94,12 @@ export async function execDryRun(bytecode, ...flags) {
   }
 }
 
-export function fromHexString(hexString) {
-  if (hexString.startsWith("0x")) hexString = hexString.slice(2)
-  return Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)))
-}
-
 export function sha256(buffer) {
   const hash = crypto.createHash('sha256')
   hash.update(buffer)
   return hash.digest('hex')
 }
 
-export function toHexString(buffer) {
-  return "0x" + Array.prototype.map.call(buffer, x => ('00' + x.toString(16)).slice(-2))
-    .join('')
-    .match(/[a-fA-F0-9]{2}/g)
-    .join('')
-}
 
 // internal helper methods ------------------------------------------------------------------------
 
@@ -103,19 +117,11 @@ function cmd (...command) {
   })
 };
 
-function isHexString(str) {
-  if (str.startsWith("0x")) str = str.slice(2)
-  return (
-    !Number.isInteger(str)
-    && /^[a-fA-F0-9]+$/i.test(str.slice(2))
-  );
-}
-
 function parseScript(array, script) {
   if (Array.isArray(array)) {
     array.forEach(item => {
       if (Array.isArray(item)) {
-        script = parseScriptOperator(script, item[0], item.slice(1))
+        script = parseScriptOperator(script, item[0], ...item.slice(1))
       } else {
         script = parseScriptOperator(script, item)
       }
@@ -145,7 +151,8 @@ function parseScriptOperator(script, opcode, args) {
     operator = operator.charAt(0).toLowerCase() + operator.slice(1)
     switch (operator) {
       case "filter": case "map": case "sort": case "alter":
-        return script[operator](parseScript(args[0]), args.slice(1));
+        var innerScript = parseScript(args)
+        return script[operator](innerScript, ...args.slice(1));
       
       // case "alter":
       //   return script[operator](args[0], parseScript(args[1], ...args.slice(2)));
@@ -154,38 +161,4 @@ function parseScriptOperator(script, opcode, args) {
         return script[operator](args)
     }
   }
-}
-
-function utf8ArrayToStr(array) {
-  var out, i, len, c;
-  var char2, char3;
-
-  out = "";
-  len = array.length;
-  i = 0;
-  while(i < len) {
-    c = array[i++];
-    switch(c >> 4)
-    { 
-      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-        // 0xxxxxxx
-        out += String.fromCharCode(c);
-        break;
-      case 12: case 13:
-        // 110x xxxx   10xx xxxx
-        char2 = array[i++];
-        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-        break;
-      case 14:
-        // 1110 xxxx  10xx xxxx  10xx xxxx
-        char2 = array[i++];
-        char3 = array[i++];
-        out += String.fromCharCode(((c & 0x0F) << 12) |
-                      ((char2 & 0x3F) << 6) |
-                      ((char3 & 0x3F) << 0));
-        break;
-    }
-  }
-
-  return out;
 }
