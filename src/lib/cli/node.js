15ced8ebbf0b311bc2d4f1f00662c1121df35921
Guillermo Díaz <guillermo@otherplane.com>
2025-01-13 11:13:25 +0100

    feat(bin): prototype node clie

---
+++ src/lib/cli/node.js
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// CLI SUBMODULE CONSTANTS ===========================================================================================
+
+module.exports = {
+    flags: {},
+    router: {
+        address: {
+            hint: "Show node address."
+        },
+        authorize: {
+            hint: "Generate stake authorization code for given withdrawer.",
+            params: "WITHDRAWER",
+        },
+        balance: {
+            hint: "Show available Wits on node.",
+        },
+        peers: {
+            hint: "List node peers.",
+        },
+        masterKey: {
+            hint: "Export node's master key.",
+        },
+        stakes: {
+            hint: "List stake entries currently delegated to the node.",
+        },
+        stats: {
+            hint: "Report node stats.",
+        },
+        status: {
+            hint: "Report current sync status.",
+        },
+    },
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// CLI SUBMODULE COMMANDS ============================================================================================
+
+
