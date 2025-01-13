9eeb9beb999908cb8968d724bfa9a343aed8b0b4
Guillermo Díaz <guillermo@otherplane.com>
2025-01-13 11:13:12 +0100

    feat(bin): prototype history cli

---
+++ src/lib/cli/history.js
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// CLI SUBMODULE CONSTANTS ===========================================================================================
+
+module.exports = {
+    flags: {
+        start: { 
+            hint: "Epoch from which search starts (default: -2048)",
+            param: ":epoch",
+        },
+        end: {
+            hint: "Epoch on which search ends (default: -1)",
+            param: ":epoch",
+        },
+        limit: { hint: "Limit output records (default: 100)", param: ":number", },
+    },
+    router: {
+        dataRequests: {
+            hint: "Report most successful data requests.",
+        },
+        validators: {
+            hint: "Report most successful validators.",
+        },
+        witnesses: {
+            hint: "Report most successful witnesses.",
+        },
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// CLI SUBMODULE COMMANDS ============================================================================================
+
+
