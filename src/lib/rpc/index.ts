3be112fd14a697720366889b5a4208e4052fb148
Guillermo Díaz <guillermo@otherplane.com>
2025-02-24 18:44:18 +0100

    feat: revisit rpc module

--- src/lib/rpc/index.ts
+++ src/lib/rpc/index.ts
@@ -1,12 +1,2 @@
-export * from "./farm"
+export * from "./nodes"
 export * from "./provider"
-export * from "./reporter"
-export * from "./wallet"
-
-export { 
-    Balance, Block, ConsensusConstants, DataRequestReport, HexString, Mempool, NodeStats,
-    PeerAddr, Priorities, ProtocolInfo, PublicKey, PublicKeyHash, PublicKeyHashString,
-    SignalingInfo, StakeAuthorization, StakeEntry, Stake, StakingCapability, StakingPower, 
-    Superblock, SupplyInfo, SyncStatus, Transaction, UtxoMetadata, Nanowits,
-} from "./types"
-
--- src/lib/rpc/provider.ts
+++ src/lib/rpc/provider.ts
@@ -1,52 +1,22 @@
-const axios = require("axios").default
-const helpers = require("../helpers")
+import * as utils from "../utils"
 
-import { AxiosHeaders } from "axios"
-import { 
-    Balance, Balance2, Block, ConsensusConstants, DataRequestReport, DataRequestTransaction, Epoch, Hash,
-    Mempool, Methods, PeerAddr, Priorities, ProtocolInfo, PublicKeyHashString, 
-    Transaction, SignalingInfo, StakeEntry, StakingCapability, StakingPower, 
-    Superblock, SyncStatus, ValueTransferTransaction, UtxoInfo, UtxoMetadata,
+import { default as axios, AxiosHeaders } from "axios"
+
+import { PublicKeyHashString, TransactionReceipt } from "../crypto/types"
+import { Epoch, Hash, Network, UtxoMetadata } from "../types"
+
+import {
+    Balance, Balance2, Block, ConsensusConstants, DataRequestReport, 
+    Mempool, Methods, PeerAddr, Priorities, ProtocolInfo, 
+    QueryStakes, QueryStakingPowers,
+    TransactionReport, SignalingInfo, StakeEntry, StakingPower, 
+    SuperblockReport, SyncStatus, UtxoInfo, 
 } from "./types"
 
-export type QueryStakes = {
-    filter?: QueryStakesFilter,
-    params?: QueryStakesParams,
-}
-
-export type QueryStakesFilter = {
-    validator?: PublicKeyHashString,
-    withdrawer?: PublicKeyHashString,
-}
-
-export type QueryStakesParams = {
-    distinct?: boolean,
-    limit?: number,
-    offset?: number,
-    order?: QueryStakesOrderBy,
-    since?: number,
-}
-
-export type QueryStakesOrderBy = {
-    by: QueryStakesOrderByOptions,
-    reverse?: boolean,
-}
-
-export enum QueryStakesOrderByOptions {
-    Coins = "coins",
-    Nonce = "nonce",
-    Mining = "mining",
-    Witnessing = "witnessing",
-}
-
-export type QueryStakingPowers = {
-    distinct?: boolean,
-    limit?: number,
-    offset?: number,
-    orderBy?: StakingCapability,
-}
-
 export interface IProvider {
+    network?: Network;
+    // receipts: Record<Hash, any>;
+    
     blocks(since: Epoch, limit: number): Promise<Array<[number, Hash/*, PublicKeyHashString*/]>>;
     constants(): Promise<ConsensusConstants>;
     holders(limit?: number): Promise<Record<PublicKeyHashString, Balance2>> 
@@ -61,12 +31,12 @@ export interface IProvider {
     getBalance(pkh: PublicKeyHashString): Promise<Balance2>;
     getBlock(blockHash: Hash, showTransactionHashes?: boolean): Promise<Block>;
     getDataRequest(drTxHash: Hash): Promise<DataRequestReport>;
-    getTransaction(txHash: Hash): Promise<Transaction>;
-    getSuperblock(epoch: Epoch): Promise<Superblock>;
+    getTransaction(txHash: Hash): Promise<TransactionReport>;
+    getTransactionReceipt(txHash: Hash): Promise<TransactionReceipt>;
+    getSuperblock(epoch: Epoch): Promise<SuperblockReport>;
     getUtxoInfo(pkh: PublicKeyHashString, smallestFirst?: boolean): Promise<Array<UtxoMetadata>>;
         
-    sendRawDataRequest(drt: DataRequestTransaction): Promise<Boolean>;
-    sendRawValueTransfer(vtt: ValueTransferTransaction): Promise<Boolean>;
+    sendRawTransaction(tx: any): Promise<boolean>;
 }
 
 export class ProviderError extends Error {
@@ -83,14 +53,26 @@ export class ProviderError extends Error {
 }
 
 export class Provider implements IProvider {
-    readonly endpoints: string[]
-    protected headers: AxiosHeaders
-    constructor(url?: string) {
+    
+    public network?: Network;
+    public static receipts: Record<Hash, TransactionReceipt> = {};
+
+    public readonly endpoints: string[]
+    
+    protected _constants?: ConsensusConstants
+    protected _headers: AxiosHeaders;
+    
+    static async initialized(url?: string): Promise<Provider> {
+        const provider = new Provider(url || "https://rpc.witnet.io")
+        return provider.constants().then(() => provider)
+    }
+
+    constructor(url: string) {
         this.endpoints = []
         if (url !== undefined) {
             const urls = url.replaceAll(',', ';').split(';')
             urls.forEach(url => {
-                const [schema, ] = helpers.parseURL(url)
+                const [schema, ] = utils.parseURL(url)
                 if (!schema.startsWith("http://") && !schema.startsWith("https://")) {
                     throw Error(`Witnet.Provider: unsupported URL schema ${schema}`)
                 }
@@ -99,7 +81,7 @@ export class Provider implements IProvider {
         } else {
             this.endpoints.push(process.env.WITNET_TOOLKIT_PROVIDER_URL || "https://rpc-01.witnet.io")
         }
-        this.headers = new AxiosHeaders({ 
+        this._headers = new AxiosHeaders({ 
             "Content-Type": "application/json" 
         })
     }
@@ -120,7 +102,7 @@ export class Provider implements IProvider {
                     params,
                 },
                 {
-                    headers: this.headers,
+                    headers: this._headers,
                 }
             
             ).then((response: any) => {
@@ -144,7 +126,17 @@ export class Provider implements IProvider {
     
     /// Get consensus constants used by the node
     public async constants(): Promise<ConsensusConstants> {
-        return this.callApiMethod<ConsensusConstants>(Methods.GetConsensusConstants);
+        if (!this._constants) {
+            return this
+                .callApiMethod<ConsensusConstants>(Methods.GetConsensusConstants)
+                .then((constants: ConsensusConstants) => {
+                    this.network = constants.bootstrapping_committee[0].startsWith('wit') ? "mainnet": "testnet"
+                    this._constants = constants
+                    return constants
+                })
+        } else {
+            return this._constants
+        }    
     }
 
     /// Get limited list of currently top holders, as known by the provider.
@@ -197,7 +189,15 @@ export class Provider implements IProvider {
     
     /// Get a full list of current stake entries  Query the amount of nanowits staked by an address.
     public async stakes(query?: QueryStakes): Promise<Array<StakeEntry>> {
-        return this.callApiMethod<Array<StakeEntry>>(Methods.QueryStakes, query)
+        return this
+            .callApiMethod<Array<StakeEntry>>(Methods.QueryStakes, query)
+            .catch(err => {
+                if (err?.error?.message && err.error.message.indexOf("not registered") > -1) {
+                    return []
+                } else {
+                    throw err
+                }
+            })
     }
     
     /// Get node status
@@ -233,20 +233,25 @@ export class Provider implements IProvider {
     }
     
     /// Get the blocks that pertain to the superblock index
-    public async getSuperblock(epoch: Epoch): Promise<Superblock> {
-        return this.callApiMethod<Superblock>(Methods.GetSuperblock, { "block_epoch": epoch });
+    public async getSuperblock(epoch: Epoch): Promise<SuperblockReport> {
+        return this.callApiMethod<SuperblockReport>(Methods.GetSuperblock, { "block_epoch": epoch });
     }
     
-    public async getTransaction(txHash: Hash): Promise<Transaction> {
-        return this.callApiMethod<Transaction>(Methods.GetTransaction, [txHash, ]);
+    public async getTransaction(txHash: Hash): Promise<TransactionReport> {
+        return this.callApiMethod<TransactionReport>(Methods.GetTransaction, [txHash, ]);
+    }
+
+    public async getTransactionReceipt(txHash: Hash): Promise<TransactionReceipt> {
+        // todo: fetch/update receipt from provider, if not cached
+        return Provider.receipts[txHash]
     }
     
     /// Get utxos
-    public getUtxoInfo(pkh: PublicKeyHashString, smallestFirst = true): Promise<Array<UtxoMetadata>> {
+    public getUtxoInfo(pkh: PublicKeyHashString, smallFirst = true): Promise<Array<UtxoMetadata>> {
         return this
             .callApiMethod<UtxoInfo>(Methods.GetUtxoInfo, [pkh, ])
             .then((result: UtxoInfo) => {
-                const inversor = smallestFirst ? 1 : -1
+                const inversor = smallFirst ? 1 : -1
                 return result.utxos.sort((a, b) => {
                     if (a.value > b.value) return inversor;
                     else if (a.value < b.value) return - inversor;
@@ -256,11 +261,7 @@ export class Provider implements IProvider {
     }
     
     /// ---------------------------------------------------------------------------------------------------------------
-    public async sendRawDataRequest(drt: DataRequestTransaction): Promise<Boolean> {
-        return this.callApiMethod<Boolean>(Methods.Inventory, { "transaction": drt })
-    }
-    
-    public async sendRawValueTransfer(vtt: ValueTransferTransaction): Promise<Boolean> {
-        return this.callApiMethod<Boolean>(Methods.Inventory, { "transaction": vtt })
+    public async sendRawTransaction(transaction: any): Promise<boolean> {
+        return this.callApiMethod<boolean>(Methods.Inventory, { transaction })
     }
 }
