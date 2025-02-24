71e6ac2f880543ad0f999b6ffcec1232e42e56f8
tommytrg <tomastrg22@gmail.com>
2025-02-24 16:25:24 +0100

    fix: import from esm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export * from "./lib/radon"
-export * from "./lib/rpc"
-export * as Utils from "./lib/utils"
+import * as Radon from "./lib/radon"
+import * as RPC from "./lib/rpc"
+import * as Utils from "./lib/utils"
+
+export { Radon, RPC, Utils }
+
+export default {
+    Radon,
+    RPC,
+    Utils
+}
+
+module.exports = {
+    Radon,
+    RPC,
+    Utils
+}
+
