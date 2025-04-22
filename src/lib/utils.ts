37728d6e76dd122a61892867de5642627cd23601
Guillermo Díaz <guillermo@otherplane.com>
2025-04-22 12:37:41 +0200

    refactor: module utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,5 +13,41 @@ export {
   utf8ArrayToStr,
 } from "../bin/helpers"
 
-export * as crypto from "./crypto/utils"
-export * as radon from "./radon/utils"
+export {
+  decipherXprv,
+  parseXprv,
+  sha256
+} from "./crypto/utils"
+
+export {
+  execRadonBytecode,
+  parseRadonScript,
+  searchRadonAssets,
+} from "./radon/utils"
+
+import { RadonModal, RadonRequest, RadonTemplate } from "./radon"
+import { flattenRadonAssets, requireRadonAsset } from "./radon/utils"
+
+export function flattenRadonRequests(assets: any): Array<RadonRequest> {
+  return flattenRadonAssets(assets, RadonRequest)
+}
+
+export function flattenRadonTemplates(assets: any): Array<RadonTemplate> {
+  return flattenRadonAssets(assets, RadonTemplate)
+}
+
+export function flattenRadonModals(assets: any): Array<RadonModal> {
+  return flattenRadonAssets(assets, RadonModal)
+}
+
+export function requireRadonRequest(artifact: string, assets?: any): RadonRequest | undefined {
+  return requireRadonAsset({ artifact, assets, type: RadonRequest })
+}
+
+export function requireRadonTemplate(artifact: string, assets?: any): RadonTemplate | undefined {
+  return requireRadonAsset({ artifact, assets, type: RadonTemplate })
+}
+
+export function requireRadonModal(artifact: string, assets?: any): RadonModal | undefined {
+  return requireRadonAsset({ artifact, assets, type: RadonModal })
+}
