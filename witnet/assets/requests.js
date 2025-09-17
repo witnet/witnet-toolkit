4ea0e6fe310095a276946667d4673e3f28b13c3f
Guillermo Díaz <guillermo@witnet.io>
2025-09-17 10:09:00 +0200

    fix(assets): wit/usdt feeders

--- witnet/assets/requests.js
+++ witnet/assets/requests.js
@@ -8,38 +8,16 @@ const {
 } = require("../../src/lib/radon")
 
 module.exports = {
-
   defi: {
     tickers: {
       crypto: {
         WitOracleRequestPriceCryptoWitUsdt6: new RadonRequest({
           sources: [
             retrievals.HttpGet({
-              url: "https://api-cloud.bitmart.com/spot/v1/ticker?symbol=WIT_USDT",
+              url: "https://api.mexc.com/api/v3/ticker/price?symbol=WITUSDT",
               script: RadonScript(types.RadonString)
                 .parseJSONMap()
-                .getMap("data")
-                .getArray("tickers")
-                .getMap(0)
-                .getFloat("last_price")
-                .multiply(1e6)
-                .round(),
-            }),
-            retrievals.HttpGet({
-              url: "https://data.gateapi.io/api2/1/ticker/wit_usdt",
-              script: RadonScript(types.RadonString)
-                .parseJSONMap()
-                .getFloat("last")
-                .multiply(1e6)
-                .round(),
-            }),
-            retrievals.HttpGet({
-              url: "https://www.mexc.com/open/api/v2/market/ticker?symbol=WIT_USDT",
-              script: RadonScript(types.RadonString)
-                .parseJSONMap()
-                .getArray("data")
-                .getMap(0)
-                .getFloat("last")
+                .getFloat("price")
                 .multiply(1e6)
                 .round(),
             }),
@@ -50,35 +28,11 @@ module.exports = {
         WitOracleRequestPriceCryptoUsdtWit9: new RadonRequest({
           sources: [
             retrievals.HttpGet({
-              url: "https://api-cloud.bitmart.com/spot/v1/ticker?symbol=WIT_USDT",
+              url: "https://api.mexc.com/api/v3/ticker/price?symbol=WITUSDT",
               script: RadonScript(types.RadonString)
                 .parseJSONMap()
-                .getMap("data")
-                .getArray("tickers")
-                .getMap(0)
-                .getFloat("last_price")
-                .power(-1)
-                .multiply(1e9)
-                .round(),
-            }),
-            retrievals.HttpGet({
-              url: "https://data.gateapi.io/api2/1/ticker/wit_usdt",
-              script: RadonScript(types.RadonString)
-                .parseJSONMap()
-                .getFloat("last")
-                .power(-1)
-                .multiply(1e9)
-                .round(),
-            }),
-            retrievals.HttpGet({
-              url: "https://www.mexc.com/open/api/v2/market/ticker?symbol=WIT_USDT",
-              script: RadonScript(types.RadonString)
-                .parseJSONMap()
-                .getArray("data")
-                .getMap(0)
-                .getFloat("last")
-                .power(-1)
-                .multiply(1e9)
+                .getFloat("price")
+                .multiply(1e6)
                 .round(),
             }),
           ],
