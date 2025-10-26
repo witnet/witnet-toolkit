fd3ac9be4ea8d94e2e4481b8bee3ab7b3c791aab
Guillermo Díaz <guillermo@witnet.io>
2025-10-26 21:35:34 +0100

    chore: convert assets to CommonJS

--- witnet/assets/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  modals: require("./modals"),
-  requests: require("./requests"),
-}
--- witnet/assets/modals/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  web3: {
-    // btc: require("./web3/btc"),
-    eth: require("./web3/eth.js"),
-    // sol: require("./web3/sol"),
-    ipfs: require("./web3/ipfs.js"),
-    wit: require("./web3/wit.js"),
-  },
-}
