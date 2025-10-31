fe6ee061ba4dace40eec40b0e656ec91da8041bb
Guillermo Díaz <guillermo@witnet.io>
2025-10-31 13:57:28 +0100

    fix: witnet/assets templates

--- witnet/assets/_index.cjs
+++ witnet/assets/_index.cjs
@@ -1,7 +1,4 @@
-const { assets } = require("@witnet/sdk")
-
 module.exports = {
-  legacy: assets,
   requests: require("./requests.cjs"),
   sources: require("./sources.cjs"),
   templates: require("./templates.cjs"),
