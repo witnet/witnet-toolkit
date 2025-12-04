ede49473a8bcb71758c3c4ced22e8c52a6124d45
Guillermo Díaz <guillermo@witnet.io>
2025-12-04 13:55:43 +0100

    fix(cli): listing unresolved data request transactions

--- src/bin/cli/inspect.js
+++ src/bin/cli/inspect.js
@@ -52,7 +52,7 @@ export const router = {
 		hint: "Search for in-flight or recently solved data request transactions.",
 		params: "RAD_BYTECODE | RAD_HASH",
 		options: {
-			limit: { hint: "Limit output records (default: 100).", param: "LIMIT" },
+			limit: { hint: "Limit output records (default: 10).", param: "LIMIT" },
 			offset: {
 				hint: "Skips first records as found on server side (default: 0).",
 				param: "SKIP",
@@ -226,7 +226,7 @@ async function dataRequests(options = {}, [arg]) {
 	// )
 	const results = await helpers.prompter(
 		provider.searchDataRequests(radHash, {
-			limit: options?.limit ? parseInt(options.limit, 10) : undefined,
+			limit: options?.limit ? parseInt(options.limit, 10) : 10,
 			offset: options?.offset ? parseInt(options.offset, 10) : undefined,
 			mode: options?.mode,
 			reverse: options?.reverse,
@@ -236,17 +236,17 @@ async function dataRequests(options = {}, [arg]) {
 		results.map((record) => {
 			let result = record?.result.cbor_bytes ? utils.cbor.decode(record?.result.cbor_bytes, { encoding: "hex" }) : "";
 			const request = Witnet.Radon.RadonRequest.fromBytecode(record.query.rad_bytecode);
-			const dataType = result.constructor.name === "Tagged" ? "RadonError" : request.dataType;
-			if (dataType !== "RadonError")
+			const dataType = result !== "" && result.constructor.name === "Tagged" ? "RadonError" : request.dataType;
+			if (result !== "" && !["RadonError", "RadonInteger", "RadonFloat"].includes(dataType))
 				result = Buffer.from(utils.fromHexString(record?.result.cbor_bytes)).toString("base64");
-			else if (result.constructor.name === "Buffer") result = result.toString("base64");
+			else if (result?.constructor.name === "Buffer") result = result.toString("base64");
 			return [
 				record.block_epoch,
 				record.hash,
 				record.query.witnesses,
 				Witnet.Coins.fromPedros(record.query.unitary_reward).toString(2),
 				dataType === "RadonError" ? helpers.colors.mred("RadonError") : helpers.colors.mgreen(dataType),
-				...(options?.verbose
+				...(options?.verbose 
 					? [
 							dataType === "RadonError"
 								? helpers.colors.red(result)
@@ -255,8 +255,8 @@ async function dataRequests(options = {}, [arg]) {
 									: helpers.colors.cyan(result),
 						]
 					: [
-							record?.result ? `${record.result.cbor_bytes.length / 2} bytes` : "",
-							record?.result.timestamp ? moment.unix(record.result.timestamp).fromNow() : "",
+							result !== "" && record?.result ? `${record.result.cbor_bytes.length / 2} bytes` : "",
+							result !== "" && record?.result.timestamp ? moment.unix(record.result.timestamp).fromNow() : "",
 						]),
 			];
 		}),
